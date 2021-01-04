f59afe0a765ee513eaa013b96beddc635eacba12
rayllanderson <rayllanderson@gmail.com>
2021-01-04 14:32:37 -0300

    Project created

---
+++ backend/src/main/java/com/rayllanderson/pedidocaneca/DsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.rayllanderson.pedidocaneca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdeliverApplication.class, args);
+	}
+
+}
