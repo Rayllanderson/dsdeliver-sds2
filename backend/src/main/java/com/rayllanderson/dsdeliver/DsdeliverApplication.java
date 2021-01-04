9a82431c9e6c8a4c83d8133e44bd13910b7379b9
rayllanderson <rayllanderson@gmail.com>
2021-01-04 15:15:48 -0300

    initial setup

--- backend/src/main/java/com/rayllanderson/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/rayllanderson/dsdeliver/DsdeliverApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsdeliverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsdeliverApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(DsdeliverApplication.class, args);
+    }
 
 }
