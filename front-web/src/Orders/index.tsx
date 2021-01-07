8aa77192bd6f60b7403796e8a51d36acc009656d
rayllanderson <rayllanderson@gmail.com>
2021-01-07 02:11:41 -0300

    front end pt1 finished

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
 import './style.css';
-import {ReactComponent as Logo} from './logo.svg'
+import { useEffect, useState } from 'react';
 import StepHeaders from './StepsHeader';
+import ProductsList from './ProductList';
+import { Product } from './types';
+import { fetchProduct } from '../api';
+
 
 function Orders(){
+
+    const[products, setProducts] = useState<Product[]>([])
+    
+    console.log(products)
+
+    useEffect (() =>{
+        fetchProduct().then(
+            response => setProducts(response.data)
+        ).catch(
+            error => console.log(error))
+    }, [])
+
     return (
        <div className="orders-container">
            <StepHeaders />
+           <ProductsList products={products} />
         </div>
     )
 }
