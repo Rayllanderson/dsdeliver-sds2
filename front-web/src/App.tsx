b8b2792923bfe039e1fa1214b58ed3e224e3a988
rayllanderson <rayllanderson@gmail.com>
2021-01-06 23:37:41 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div className="App">
-        <h1>Hey Dear!</h1>
+        <Navbar />
     </div>
   );
 }
