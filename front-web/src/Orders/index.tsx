8e3d4a2a67e9e3426a5a2c0bc7b307db36a8230a
rayllanderson <rayllanderson@gmail.com>
2021-01-07 01:08:34 -0300

    Add routes and StepsHeaders

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+import {ReactComponent as Logo} from './logo.svg'
+import StepHeaders from './StepsHeader';
+
+function Orders(){
+    return (
+       <div className="orders-container">
+           <StepHeaders />
+        </div>
+    )
+}
+
+export default Orders;
